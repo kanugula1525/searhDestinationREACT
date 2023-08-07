6d09329c8761e5ec057d02b564f312d16bb4bcd1
Your Name <you@example.com>
2023-08-07 04:58:54 +0000

    second commit resolved code

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,42 +1,45 @@
 // Write your code here
+import {Component} from 'react'
+
 import './index.css'
 import DestinationItem from '../DestinationItem/index'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
 
-  let searchInput = ''
+  onChangeSearch = event => this.setState({searchInput: event.target.value})
 
-  const onSearchChange = event => {
-    searchInput = event.targe.value
-  }
-  const searchResult = destinationsList.filter(eachDest =>
-    eachDest.name.includes(searchInput),
-  )
-
-  return (
-    <div className="mainContainer">
-      <h1>Destination Search</h1>
-      <div className="searchContainer">
-        <input
-          onChange={onSearchChange}
-          className="inputStyle"
-          type="search"
-          placeholder="Search"
-        />
-        <img
-          className="searchIcon"
-          src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-          alt="search icon"
-        />
+  render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+    const filteredDestinations = destinationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="mainContainer">
+        <h1>Destination Search</h1>
+        <div className="searchContainer">
+          <input
+            className="inputStyle"
+            type="search"
+            placeholder="Search"
+            onChange={this.onChangeSearch}
+            value={searchInput}
+          />
+          <img
+            className="searchIcon"
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+          />
+        </div>
+        <ul className="destinationItemsContainer">
+          {filteredDestinations.map(eachItem => (
+            <DestinationItem key={eachItem.id} eachItem={eachItem} />
+          ))}
+        </ul>
       </div>
-      <ul className="destinationItemsContainer">
-        {searchResult.map(eachItem => (
-          <DestinationItem eachItem={eachItem} />
-        ))}
-      </ul>
-    </div>
-  )
+    )
+  }
 }
 
 export default DestinationSearch
